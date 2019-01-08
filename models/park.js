46c25146f07e08ab42a6bac59346c89ef86a0f8b
Gerry Waterston <gwaterston@hotmail.co.uk>
2019-01-08 23:16:43 +0000

    Set-up test for findDinosaur

--- models/park.js
+++ models/park.js
@@ -10,12 +10,24 @@ Park.prototype.addDinosaur = function(dinosaurName) {
   this.dinoCollection.push(dinosaurName);
 };
 
-// Park.prototype.removeDinosaur = function(dinosaurName) {
-//   // const indexOfDinos = this.dinoCollection.indexOf(dinosaurName);
-//   this.dinoCollection.splice(this.dinoCollection.indexOf(dinosaurName), 1);
-// };
+Park.prototype.removeDinosaur = function(dinosaurName) {
+const indexOfDinos = this.dinoCollection.indexOf(dinosaurName);
+  this.dinoCollection.splice(indexOfDinos, 1);
+};
 
+Park.prototype.findDinosaur = function (speciesName) {
+  const foundSpecies = [];
 
+  for(const species in this.dinoCollection) {
+    if(species === speciesName) {
+      foundSpecies.push(species);
+    }
+  }
+};
 
 
 module.exports = Park
+
+// var found = array1.find(function(element) {
+//   return element > 10;
+// });
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -9,11 +9,11 @@ describe('Park', function() {
   beforeEach(function () {
     dino1 = new Dinosaur('spinosaurus', 'carnivore', 75);
     dino2 = new Dinosaur('brachiosaurus', 'herbivore', 100);
-    dino3 = new Dinosaur('Avimimus', 'omnivore', 200);
-    dino4 = new Dinosaur('Allosaurus', 'carnivore', 300);
+    dino3 = new Dinosaur('avimimus', 'omnivore', 200);
+    dino4 = new Dinosaur('allosaurus', 'carnivore', 300);
     dinoCollection = [dino1, dino2, dino3];
     park = new Park('Jurassic Park', 10, dinoCollection);
-})
+  })
 
   it('should have a name', function() {
     const actual = park.name;
@@ -25,11 +25,11 @@ describe('Park', function() {
     assert.strictEqual(actual, 10);
   });
 
-//  test empty array first
-  xit('should have a collection of dinosaurs', function() {
-    const actual = park.dinoCollection;
-    assert.deepStrictEqual(actual, [])
-  });
+// //  test empty array first
+//   xit('should have a collection of dinosaurs', function() {
+//     const actual = park.dinoCollection;
+//     assert.deepStrictEqual(actual, [])
+//   });
 
   it('should have a collection of dinosaurs', function() {
     const actual = park.dinoCollection;
@@ -40,23 +40,35 @@ describe('Park', function() {
   it('should be able to add a dinosaur to its collection', function() {
     park.addDinosaur(dino4);
     const actual = park.dinoCollection.length;
-    assert.strictEqual(actual, 4);
+    assert.deepStrictEqual(actual, 4);
   });
 
   it('should be able to remove a dinosaur from its collection', function() {
     park.addDinosaur(dino4);
     park.removeDinosaur(dino3);
     const actual = park.dinoCollection.length;
-    assert.strictEqual(actual, 3);
+    assert.deepStrictEqual(actual, 3);
+  });
+
+  // xit('should be able to find the dinosaur that attracts the most visitors'), function() {
+  // park.dinoCollection;
+  // const actual = park.mostVisitedDino(dino4)
+  // assert.strictEqual(actual, 'allosaurus')
+  // });
+
+  it('should be able to find all dinosaurs of a particular species', function() {
+  park.dinoCollection;
+  const actual = park.findDinosaur(dino2)
+  assert.strictEqual(actual, 'brachiosaurus')
+  });
+
+// it('should be able to remove all dinosaurs of a particular species', function() {
+//   park.dinoCollection;
+//   const actual
+// });
+//
 });
 
-  // it('should be able to find the dinosaur that attracts the most visitors'), function() {
-
-  // it('should be able to find all dinosaurs of a particular species');
-  //
-  // it('should be able to remove all dinosaurs of a particular species');
-
-});
 
 
 // A park must have:
@@ -67,7 +79,7 @@ describe('Park', function() {
 //
 // A park must be able to:
 // Add a dinosaur to its collection of dinosaurs - DONE
-// Remove a dinosaur from its collection of dinosaurs - METHOD NOT WORKING
+// Remove a dinosaur from its collection of dinosaurs - DONE
 // Find the dinosaur that attracts the most visitors
 // Find all dinosaurs of a particular species
 // Calculate the total number of visitors per day
